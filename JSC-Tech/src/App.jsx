6896092004a403bcff026e2f03f6ce8ea18bf7d2
Devika <dech8707@gmail.com>
2026-08-08 18:46:46 +0530

    Header Created

--- JSC-Tech/src/App.jsx
+++ JSC-Tech/src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Contact from "./Pages/Contact"
+import Login from "./Pages/Login"
+import {Routes, Route } from "react-router-dom";
+import Header from "./Components/Header"
 
 const App = () => {
   return(
-    <div></div>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </div>
   )
 }
 export default App
---
+++ JSC-Tech/src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+const Header = () => {
+  return(
+    <div>
+      <div className="flex justify-between px-14 border border-black py-4 ">
+        <div className="text-5xl font-bold text-[#13418B]">JSC</div>
+        <div className="flex juatify-between items-center ">
+          <ul className="flex gap-10 mr-10 font-bold text-lg text-[#4A4D55]">
+            <li><Link to="/" className="hover:text-[#061F3F] cursor-pointer hover:border-b border-[#13418B]">Home</Link></li>
+            <li><Link to="/about" className="hover:text-[#061F3F] hover:border-b border-[#13418B] cursor-pointer">About</Link></li>
+            <li><Link to="" className="hover:text-[#061F3F] hover:border-b border-[#13418B]  cursor-pointer">Services</Link></li>
+            <li><Link to="/contact" className="hover:text-[#061F3F] hover:border-b border-[#13418B]  cursor-pointer">Contact</Link></li>
+            <li><Link to="/login" className="hover:text-[#061F3F] hover:border-b border-[#13418B]  cursor-pointer">Login</Link></li>
+          </ul>
+          <button className="border border-[#13418B] bg-[#13418B] text-white px-6 py-2 rounded-sm">Get Started</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
